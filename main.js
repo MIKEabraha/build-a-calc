47bc0f6d0ff08f258f597267a76c2f3f76694fcb
MIKEabraha <65959809+MIKEabraha@users.noreply.github.com>
2020-09-04 13:25:20 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function dis (value){
+document.getElementById("edu").value+=value;
+}
+
+function solve(){
+let x = document.getElementById("edu").value;
+let y = eval(x);
+document.getElementById("edu").value = y;
+}
+
+function clr(){
+document.getElementById("edu").value = "";
+
+
+}
